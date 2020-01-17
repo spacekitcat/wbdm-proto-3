1cf012ae26028d56414e7fd48ebebe1c2fd9d0ef
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2020-01-17 23:54:01 +0000

    sequences four notes on request (calculated from a hardcoded bpm and time sig values) little better than master

--- page.jsx
+++ page.jsx
@@ -2,9 +2,21 @@ class Page extends React.Component {
   constructor(props) {
     super(props);
 
+    this.bpm = 120;
+    this.timeSeconds = 60;
+    this.quarterNotesPerBar = 4;
+
     this.state = { isLoaded: false, playbackQueue: [], current: '', currentSample: null };
     this.playDrumsEventHandler = this.playDrumsEventHandler.bind(this);
     this.timeoutHandler = this.timeoutHandler.bind(this);
+  }
+
+  calculateNoteInterval() {
+    return this.timeSeconds / this.bpm;
+  }
+
+  calculateBarLength() {
+    return this.calculateNoteInterval() * this.quarterNotesPerBar;
   }
 
   initWebAudio() {
@@ -21,10 +33,35 @@ class Page extends React.Component {
 
     console.log('bang your drum!');
     let sample = selectRandomSample();
-    let when = this.audioContext.currentTime + 4;
+    let sample2 = selectRandomSample();
+    let sample3 = selectRandomSample();
+    let sample4 = selectRandomSample();
+    let when = this.audioContext.currentTime + (1 * this.quarterNotesPerBar * this.calculateNoteInterval());
     console.log(when);
     playSample(this.audioContext, sample.audioData, when);
+    playSample(
+      this.audioContext,
+      sample2.audioData,
+      when + this.calculateNoteInterval()
+    );
+    playSample(
+      this.audioContext,
+      sample3.audioData,
+      when + 2 * this.calculateNoteInterval()
+    );
+
+    playSample(
+      this.audioContext,
+      sample4.audioData,
+      when + 3 * this.calculateNoteInterval()
+    );
     playbackQueue.push({ sample, when });
+    playbackQueue.push({ sample: sample2, when: when + this.calculateNoteInterval() });
+    playbackQueue.push({ sample: sample3, when: when + 2 * this.calculateNoteInterval() });
+    playbackQueue.push({
+      sample: sample4,
+      when: when + 3 * this.calculateNoteInterval()
+    });
     this.setState({ playbackQueue });
   }
 
@@ -61,7 +98,7 @@ class Page extends React.Component {
         this.setState({ isLoaded: true });
       })
       .then(() => {
-        window.setInterval(this.timeoutHandler, 300);
+        window.setInterval(this.timeoutHandler, 200);
       });
   }
 
